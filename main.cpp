81afa1ef1e52eb607eda95a8e741550daed442b0
Lion Kortlepel <development@kortlepel.com>
2020-04-13 18:11:58 +0200

    add double_equals

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <cassert>
+
+static constexpr bool double_equals(double a, double b, double precision = 1e-10) {
+    return (a - b < 0 ? -(a - b) : a - b) < precision;
+}
 
 int main(int, char**) {
-    std::cout << "Hello, precision!" << std::endl;
+    // ghetto tests
+
+    // these are the same with precision of 5
+    assert(double_equals(0.000001, 0.000009, 1e-5));
+    // these are not
+    assert(!double_equals(0.000001, 0.000019, 1e-5));
 }
